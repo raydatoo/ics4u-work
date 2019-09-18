07e7e4ce846821c9d292eae41225bade5d707d7e
raydatoo <rayhaandatoo@hotmail.com>
2019-09-18 17:43:10 -0400

    H

---
+++ read.py
@@ -0,0 +1,33 @@
+selecttheclass
+
+    dispaly class.all attributes
+    course code
+    course name
+    period
+    teacher name
+    student list
+    assignments list
+
+    what would u like to do:
+    -tweak code
+    -tweak period
+    -tweak teacher
+    -tweak name
+        What would you like to chnaange blank to? 
+        Thank you, chnages have been saved.
+
+    -view student list
+        Which student woulf you like to view?
+            display student.all attributes
+                what would you like to do? 
+                    tweak whatever:
+                    go back:
+
+    -view assignment list
+        which assignment would you like to view?
+            tweak whatever:
+            go back
+
+
+
+
--- terminal-activity/test/goodbye.py
+++ terminal-activity/test/goodbye.py
@@ -1,1 +1,1 @@
-print("hello")
+code print("hello")
