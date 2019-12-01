a8040678532f1997eb6d8fdf76ce4b59f17b062e
raydatoo <rayhaandatoo@hotmail.com>
2019-12-01 15:21:59 -0500

    wjdhgdwgh

--- algorithams/liner_search.py
+++ algorithams/liner_search.py
@@ -25,8 +25,31 @@ def function_3(liist, target):
 
 
 
-#print(function_3([1,2,4,5,6,4,2,3,5,6,4,8], 4))
-print(function_2([1,2,4,5,6,4,2,3,5,6,4,8], 4))
+print(function_3([1], 4))
+#print(function_2([1,2,4,5,6,4,2,3,5,6,4,8], 4))
+
+
+
+
+
+import math
+
+
+def binary_search(target, lista):
+
+    start = 0
+    end = len(lista) - 1
+
+    while start <= end:
+        mid = math.ceil((start + end) // 2)
+        if lista[mid] == target:
+            return mid
+        elif lista[mid] > target:
+            end = mid - 1
+        elif lista[mid] < target:
+            start = mid + 1
+
+    return -1
 
 
 
