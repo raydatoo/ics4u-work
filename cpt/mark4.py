c40a726fa97f379ab38b7818971bcc800b18e876
raydatoo <rayhaandatoo@hotmail.com>
2020-01-04 20:09:33 -0500

    d

--- cpt/mark4.py
+++ cpt/mark4.py
@@ -1,1 +1,13 @@
 #fit mark three to native chromebook resolution
+
+'''
+adjustments needed
+-------------------
+width and height
+heart and coin pos
+player start pos
+player boundaries
+turret pos
+score pos
+'''
+
